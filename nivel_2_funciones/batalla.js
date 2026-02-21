bbffca05f51736df77dd7c59f9a460062ee47f5e
MaricieloPeralta <peraltacielito@gmail.com>
2026-02-21 15:52:10 -0500

    Update batalla.js

--- nivel_2_funciones/batalla.js
+++ nivel_2_funciones/batalla.js
@@ -1,3 +1,19 @@
 function ataque(){
     console.log("Batman sale al ataque de su baticueva")
 }
+
+
+function recargarEnergia(){
+    console.log("Batman recarga su energía")
+}
+
+
+
+
+
+
+
+
+ataque()
+
+recargarEnergia()
