e38ba6190eb5c476ff6c9577688ff8f1d493381c
kmy1 <98854090+kmly2@users.noreply.github.com>
2026-02-21 15:10:58 -0500

    update : modificar enemigo

--- nivel_1_variables/heroe2.js
+++ nivel_1_variables/heroe2.js
@@ -2,7 +2,7 @@ const nombreHeroe = "Batman";
 const poder = "Aparatos de todo tipo?"
 let nivelEnergia = 90
 const ciudad = "Ciudad Gótica"
-const enemigo = "Jocker"
+const enemigo = "El Guasón"
 
 console.log(`${nombreHeroe} se fue a comer un helado en su incríble ${ciudad}`);
 console.log(`Sus ${poder} nunca pudieron ser derribados por nadie.`);
