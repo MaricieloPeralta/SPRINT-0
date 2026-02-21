38178f97884baac0e806149d3cd03a6d290a72fe
MaricieloPeralta <peraltacielito@gmail.com>
2026-02-21 15:05:36 -0500

    Create heroe2.js

---
+++ nivel_1_variables/heroe2.js
@@ -0,0 +1,9 @@
+const nombreHeroe = "Batman";
+const poder = "Aparatos de todo tipo?"
+let nivelEnergia = 88
+const ciudad = "Ciudad Gótica"
+const enemigo = "Jocker"
+
+console.log(`${nombreHeroe} se fue a comer un helado en su incríble ${ciudad}`);
+console.log(`Sus ${poder} nunca pudieron ser derribados por nadie.`);
+console.log(`Su nivel de energía descendió hasta ${nivelEnergia} y el ${enemigo} encontró su oportunidad para vencerlo.`);
