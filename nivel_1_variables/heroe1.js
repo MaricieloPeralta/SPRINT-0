36f90f63be931700046cb1e89ae0f6f6c6e38faf
kmy1 <98854090+kmly2@users.noreply.github.com>
2026-02-21 15:07:49 -0500

    feat: agregar heroe1.js

---
+++ nivel_1_variables/heroe1.js
@@ -0,0 +1,11 @@
+const nombreHeroe = "Naruto Uzumaki";
+const poder = "Rasengan y clones de sombra";
+let nivelEnergia = 90;
+const ciudad = "Aldea Oculta de la Hoja";
+const enemigo = "Madara Uchiha";
+
+console.log(`${nombreHeroe} está protegiendo la ${ciudad}.`);
+console.log(`Su poder es ${poder}.`);
+console.log(`Tiene un nivel de energía de ${nivelEnergia}.`);
+console.log(`Su enemigo es ${enemigo}.`);
+console.log(`${nombreHeroe} se prepara para luchar contra ${enemigo}.`);
