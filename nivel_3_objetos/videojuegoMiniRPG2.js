a763540a0308aaa12992b63d7d355a3f82be6209
kmy1 <98854090+kmly2@users.noreply.github.com>
2026-02-23 10:27:56 -0500

    feat: agregar funcion subirNivel

--- nivel_3_objetos/videojuegoMiniRPG2.js
+++ nivel_3_objetos/videojuegoMiniRPG2.js
@@ -14,3 +14,10 @@ function recibirDmg(jugador) {
     jugador.vida -= 20;
     console.log(`${jugador.nombre} recibe daño. Vida actual: ${jugador.vida}`);
 }
+
+function subirNivel(jugador) {
+    jugador.nivel++;
+    jugador.fuerza += 15;
+    jugador.vida += 25;
+    console.log(`${jugador.nombre} subió al nivel ${jugador.nivel}!`);
+}
