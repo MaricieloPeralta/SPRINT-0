89a346033ca25af4812e41d51b5d91490d640985
MaricieloPeralta <peraltacielito@gmail.com>
2026-02-23 09:16:00 -0500

    Update Restaurante1.js

--- nivel_3_objetos/Restaurante1.js
+++ nivel_3_objetos/Restaurante1.js
@@ -3,3 +3,23 @@ let cliente = {
     dinero: 50,
     hambre: 100
 };
+function ordenarHamburguesa(cliente) {
+    if (cliente.dinero >= 20) {
+        cliente.dinero -= 20;
+        cliente.hambre -= 30;
+        console.log(cliente.nombre + " ordenó Hamburguesa!");
+    } else {
+        console.log("No tiene suficiente dinero");
+    }
+}
+function pagar(cliente) {
+    console.log("Ahora " + cliente.nombre + "tiene " + cliente.dinero)
+}
+function mostrarEstadoCliente(cliente) {
+    console.log("Estado Actual")
+    console.log("Dinero: " + cliente.dinero);
+    console.log("Hambre: " + cliente.hambre);
+}
+ordenarHamburguesa(cliente);
+pagar(cliente);
+mostrarEstadoCliente(cliente);
