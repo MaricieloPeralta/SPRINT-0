371b85caa1f70fdb7fa815d0a1f6eec9cf25a323
kmy1 <98854090+kmly2@users.noreply.github.com>
2026-02-23 10:24:25 -0500

    feat: agregar funcion atacar

--- nivel_3_objetos/videojuegoMiniRPG2.js
+++ nivel_3_objetos/videojuegoMiniRPG2.js
@@ -4,3 +4,8 @@ const jugador = {
     fuerza: 75,
     nivel: 3
 };
+
+function atacar(jugador) {
+    jugador.fuerza += 10;
+    console.log(`${jugador.nombre} ataca!. Fuerza actual: ${jugador.fuerza}`);
+}
