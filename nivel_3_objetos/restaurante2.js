c6aba54fe0d2ab964e8b6c77876a885cb1d8428c
kmy1 <98854090+kmly2@users.noreply.github.com>
2026-02-23 09:05:29 -0500

    feat: agregar funcion pagar

--- nivel_3_objetos/restaurante2.js
+++ nivel_3_objetos/restaurante2.js
@@ -8,3 +8,13 @@ function ordenarComida(cliente) {
     cliente.hambre -= 30;
     console.log(`${cliente.nombre} ordenó su comida. Hambre actual: ${cliente.hambre}`);
 }
+
+function pagar(cliente) {
+    if (cliente.dinero >= 15) {
+        const precioComida = 15;
+        cliente.dinero -= precioComida;
+        console.log(`${cliente.nombre} pagó S/.${precioComida}. Dinero restante: S/.${cliente.dinero}`);
+    } else {
+        console.log(`${cliente.nombre} no tiene dinero suficiente para pagar la cuenta`);
+    }
+}
