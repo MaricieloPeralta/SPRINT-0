5c341ac98e58033094733d3c0748dd0dfeb1c640
MaricieloPeralta <peraltacielito@gmail.com>
2026-02-23 10:16:04 -0500

    Update videojuegoMiniRPG.js

--- nivel_3_objetos/videojuegoMiniRPG.js
+++ nivel_3_objetos/videojuegoMiniRPG.js
@@ -12,3 +12,20 @@ function ataqueFuerte(jugador) {
     jugador.fuerza -= 20;
     console.log(`${jugador.nombre} hizo un ATAQUE FUERTE y su fuerza restante es de ${jugador.fuerza}`);
 }
+function comerManzanaDorada(jugador) {
+    jugador.fuerza += 15;
+    console.log(`${jugador.nombre} ha ingerido MANZANA DORADA! Ahora tiene ${jugador.fuerza} de fuerza.`)
+}
+function posionMagica(jugador) {
+    jugador.vida++;
+    console.log(`${jugador.nombre} tomó POSIÓN MÁGICA, ahora tiene ${jugador.vida} vidas restantes.`);
+}
+function matarEnemigo(jugador) {
+    jugador.nivel++;
+    console.log(`${jugador.nombre} derrotó al enemigo! ${jugador.nombre} ha SUBIDO DE NIVEL A ${jugador.nivel}`);
+}
+ataqueNormal(jugador)
+ataqueFuerte(jugador)
+comerManzanaDorada(jugador)
+posionMagica(jugador)
+matarEnemigo(jugador)
