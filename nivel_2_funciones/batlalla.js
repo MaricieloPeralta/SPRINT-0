83dc8f7a2e4d1c3b802dc6acc6db963006a5018b
kmy1 <98854090+kmly2@users.noreply.github.com>
2026-02-23 08:21:03 -0500

    feat: agregar funcion defender

--- nivel_2_funciones/batlalla.js
+++ nivel_2_funciones/batlalla.js
@@ -8,3 +8,8 @@ function atacar() {
     energiaEnemigo -= 10;
     console.log(${heroe} ataca a ${enemigo}. Energía restante: ${energiaEnemigo});
 }
+
+function defender() {
+    energiaHeroe += 5;
+    console.log(${heroe} se defiende. Energía restante: ${energiaHeroe});
+}
