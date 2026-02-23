e2381e884487336b7095114fea7a6709ccd843ad
kmy1 <98854090+kmly2@users.noreply.github.com>
2026-02-23 08:29:45 -0500

    feat: agregar funcion superAtaque

--- nivel_2_funciones/batlalla.js
+++ nivel_2_funciones/batlalla.js
@@ -16,6 +16,12 @@ function recargarEnergia() {
     energiaHeroe += 10;
     console.log(`${heroe} recarga energía. Energía actual: ${energiaHeroe}`);
 }
+
+function superAtaque() {
+    energiaEnemigo -= 25;
+    console.log(`${heroe} usa su Super Ataque contra ${enemigo}. Energía restante: ${energiaEnemigo}`);
+}
+
 function escudoMagico() {
     energiaHeroe += 15;
     console.log(`${heroe} recarga energía. Energía actual: ${energiaHeroe} `)
