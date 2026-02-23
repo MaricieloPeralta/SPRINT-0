e9d77cb2af20a3b6be5678f7c1b94216d4e60745
kmy1 <98854090+kmly2@users.noreply.github.com>
2026-02-23 09:04:10 -0500

    feat: agregar funcion ordenarComida

--- nivel_3_objetos/restaurante2.js
+++ nivel_3_objetos/restaurante2.js
@@ -3,3 +3,8 @@ const cliente = {
     dinero: 100,
     hambre: 50
 };
+
+function ordenarComida(cliente) {
+    cliente.hambre -= 30;
+    console.log(`${cliente.nombre} ordenó su comida. Hambre actual: ${cliente.hambre}`);
+}
