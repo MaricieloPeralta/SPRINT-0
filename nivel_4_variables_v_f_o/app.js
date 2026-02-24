b7b33ad7229c019942794f81912e9062eabf90ff
kmy1 <98854090+kmly2@users.noreply.github.com>
2026-02-24 13:18:36 -0500

    Mejora en renderMenu para mostrar total

--- nivel_4_variables_v_f_o/app.js
+++ nivel_4_variables_v_f_o/app.js
@@ -39,9 +39,9 @@ function renderMenu() {
    const plato = menu[i];
    html += `<li>${plato.nombre} — S/ ${plato.precio} — Stock: ${plato.stock}</li>`;
  }
-
- html += "</ul>";
- output.innerHTML = html;
+    html += "</ul>";
+    output.innerHTML = html;
+    html += `<p>Total de platos en el menú: ${menu.length}</p>`;
 }
 
 // 3) FUNCIÓN: agregar un plato demo al menú
