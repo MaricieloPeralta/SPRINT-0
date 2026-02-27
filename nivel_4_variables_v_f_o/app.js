a45402bef21b1294cc95e8ae03ae8b94431772cb
kmy1 <kmychavez760@gmail.com>
2026-02-27 10:58:14 -0500

    Mejora en el btnComprar

--- nivel_4_variables_v_f_o/app.js
+++ nivel_4_variables_v_f_o/app.js
@@ -182,18 +182,23 @@ document.getElementById("btnBuscarTiendita").addEventListener("click", () => {
 document.getElementById("btnComprar").addEventListener("click", () => {
     const cantidad = Number(document.getElementById("inputCantidad").value);
     const output = document.getElementById("outputTiendita");
+    const btnFinal = document.getElementById("btnMostrarMenuDeNuevo");
 
     if (cantidad < 1) {
         alert("Ingresa una cantidad válida");
+        btnFinal.style.display = "none";
         return;
     }
-
+    else {
+        btnFinal.style.display = "block";
+    }
     // buscar el plato de nuevo
     const texto = document.getElementById("inputBuscarTiendita").value.toLowerCase();
     const plato = menu.find(p => p.nombre.toLowerCase().includes(texto));
 
     if (plato.stock < cantidad) {
         alert("No hay suficiente stock");
+        divFinal.style.display = "none";
         return;
     }
 
