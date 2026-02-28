44fc88551ab24ebc0b24bb9f2176ef9cff99da47
kmy1 <kmychavez760@gmail.com>
2026-02-28 15:09:15 -0500

    Mover funcion renderMenu() a ui.js

--- nivel_4_variables_v_f_o/ui.js
+++ nivel_4_variables_v_f_o/ui.js
@@ -6,3 +6,27 @@ import {
     verificarEstadoGeneral,
     venderPlato
 } from "./operaciones.js";
+
+export function renderMenu() {
+    const output = document.getElementById("output");
+    output.innerHTML = "";
+
+    let html = "<ul>";
+    for (let i = 0; i < menu.length; i++) {
+        const plato = menu[i];
+        let clase = "";
+        if (plato.stock == 0) {
+            clase = "agotado";
+            html += `<li class="${clase}">${plato.nombre} — S/ ${plato.precio} — Stock: ${plato.stock} - Estado: ${clase}</li>`;
+        } else if (plato.stock <= 3) {
+            clase = "bajo";
+            html += `<li class="${clase}">${plato.nombre} — S/ ${plato.precio} — Stock: ${plato.stock} - Estado: ${clase}</li>`;
+        } else {
+            clase = "normal";
+            html += `<li class="${clase}">${plato.nombre} — S/ ${plato.precio} — Stock: ${plato.stock} - Estado: ${clase}</li>`;
+        }
+    }
+    html += "</ul>";
+    html += `<p>Total de platos en el menú: ${menu.length}</p>`;
+    output.innerHTML = html;
+}
