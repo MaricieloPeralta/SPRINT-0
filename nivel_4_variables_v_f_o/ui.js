3bd43a113d59fc97ff820a1d1d10770508aa3ed3
kmy1 <kmychavez760@gmail.com>
2026-03-02 14:21:55 -0500

    agregar modificacion al btnBuscar

--- nivel_4_variables_v_f_o/ui.js
+++ nivel_4_variables_v_f_o/ui.js
@@ -56,16 +56,21 @@ export function conectarEventos() {
 
     document.getElementById("btnBuscar").addEventListener("click", () => {
         const input = document.getElementById("inputBuscar").value;
-        const plato = buscarPlatoPorNombre(input);
-        const output = document.getElementById("output2");
 
-        if (plato) {
-            output.innerHTML = `<ul><li>${plato.nombre} — S/ ${plato.precio} — Stock: ${plato.stock}</li></ul>`;
-        } else {
-            output.innerHTML = "<p>No se encontraron resultados</p>";
+        if (!input) {
+            alert("Por favor, ingresa un nombre de plato.");
+            return;
+        }
+        else {
+            const plato = buscarPlatoPorNombre(input);
+            const output = document.getElementById("output2");
+            if (plato) {
+                output.innerHTML = `<ul><li>${plato.nombre} — S/ ${plato.precio} — Stock: ${plato.stock}</li></ul>`;
+            } else {
+                output.innerHTML = "<p>No se encontraron resultados</p>";
+            }
         }
     });
-
 
     document.getElementById("btnStockBajo").addEventListener("click", () => {
         const platosStockBajo = filtrarStockBajo();
